f9396154c2166dcbf2f753c30fd72792d5abfb4c
Mark Pascua <markpascua680@yahoo.com>
2022-08-17 09:49:59 -0800

    Displayed background and moving fish

---
+++ Fishing Game/interface.cpp
@@ -0,0 +1,212 @@
+#include <iostream>
+#include "interface.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////	CLASS BUTTON DEFINITIONS	////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+Button::Button() {
+
+}
+
+Button::Button(SDL_Rect sRect, SDL_Rect hRect, SDL_Rect dRect, std::string path) :
+	srcRect(sRect),
+	hoverRect(hRect),
+	dstRect(dRect),
+	filePath(path)
+{}
+
+Button::~Button() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////	CLASS INTERFACE DEFINITIONS	   /////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+Interface::Interface() {
+
+	_font = NULL;
+	_fontSize = 0;
+
+	// End program if fail to initialize
+	if (SDL_Init(SDL_INIT_EVERYTHING < 0)) {
+		std::cout << "UNABLE TO SDL_INIT_EVERYTHING" << std::endl;
+		EXIT_FAILURE;
+	}
+
+	if (IMG_Init(IMG_INIT_PNG) < 0) {
+		std::cout << "UNABLE TO IMG_INIT" << std::endl;
+		EXIT_FAILURE;
+	}
+
+	if (TTF_Init() < 0) {
+		std::cout << "UNABLE TO TTF_INIT" << std::endl;
+		EXIT_FAILURE;
+	}
+
+	// Create window
+	_window = SDL_CreateWindow("Fishing", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+
+	if (!_window) {
+		std::cout << "UNABLE TO CREATE WINDOW" << std::endl;
+		EXIT_FAILURE;
+	}
+
+	// Get window surface
+	_surface = SDL_GetWindowSurface(_window);
+
+	// Set surface color to white
+	SDL_FillRect(_surface, NULL, SDL_MapRGB(_surface->format, 255, 255, 255));
+
+	// Create renderer
+	_renderer = SDL_CreateRenderer(_window, -1, 0);
+
+	// Fill the screen with color white
+	_texture = SDL_CreateTextureFromSurface(_renderer, _surface);
+	SDL_RenderCopy(_renderer, _texture, NULL, NULL);
+
+	SDL_FreeSurface(_surface);
+}
+
+Interface::~Interface() {
+
+	SDL_DestroyWindow(_window);
+}
+
+// Takes rect of image, rect of destination on screen, and image file path
+void Interface::render(SDL_Rect* clip, SDL_Rect* dstRect, std::string path) {
+
+	SDL_Surface* image = IMG_Load(path.c_str());
+	if (image == NULL)
+		std::cout << "UNABLE TO OPEN IMAGE" << std::endl;
+
+	// Then render image over background
+	_texture = SDL_CreateTextureFromSurface(_renderer, image);
+	SDL_RenderCopy(_renderer, _texture, clip, dstRect);
+
+	SDL_DestroyTexture(_texture);
+	SDL_FreeSurface(image);
+}
+
+// Flips image
+void Interface::renderFlip(SDL_Rect* clip, SDL_Rect* dstRect, std::string path, double angle, const SDL_Point* center, SDL_RendererFlip flip) {
+
+	SDL_Surface* image = IMG_Load(path.c_str());
+	if (image == NULL)
+		std::cout << "UNABLE TO OPEN IMAGE" << std::endl;
+
+	// Then render image over background
+	_texture = SDL_CreateTextureFromSurface(_renderer, image);
+	SDL_RenderCopyEx(_renderer, _texture, clip, dstRect, angle, center, flip);
+
+	SDL_DestroyTexture(_texture);
+	SDL_FreeSurface(image);
+}
+
+// Clear Renderer
+void Interface::clear() {
+
+	SDL_RenderClear(_renderer);
+}
+
+// Updates screen
+void Interface::update() {
+
+	// Display rendered images
+	SDL_RenderPresent(_renderer);
+
+}
+
+void Interface::setWindowWidth(int w) {
+
+	this->WINDOW_WIDTH = w;
+}
+void Interface::setWindowHeight(int h) {
+	
+	this->WINDOW_HEIGHT = h;
+}
+
+// Saves a button's original image, image when it's hovered and the screen destination that it will be displayed at
+void Interface::addButton(std::string button, SDL_Rect* srcRect, SDL_Rect* _hoverRect, SDL_Rect* dstRect, std::string filePath) {
+
+	Button b = { *srcRect, *_hoverRect, *dstRect,  filePath };
+	buttons[button] = b;
+}
+
+// Takes the name of the button
+void Interface::displayButton(std::string button) {
+	
+	Button b = buttons[button];
+
+	SDL_GetMouseState(&mousePos.x, &mousePos.y);
+
+	// Display the button's hover image if mouse is over it, display original image if not
+	if (isButtonHovered(mousePos, button))
+		render(&b.hoverRect, &b.dstRect, b.filePath);
+	else
+		render(&b.srcRect, &b.dstRect, b.filePath);
+}
+
+// Returns true if mouse is hovering over button
+bool Interface::isButtonHovered(SDL_Point mousePos, std::string button) {
+
+	Button b = buttons[button];
+	if (SDL_PointInRect(&mousePos, &b.dstRect)) {
+		return true;
+	}
+
+	return false;
+}
+
+// Saves the image's file path and rectangle
+void Interface::addImage(std::string name, SDL_Rect* rect, std::string filePath) {
+
+	Image i = { filePath, *rect };
+	images[name] = i;
+}
+
+void Interface::displayImage(std::string name) {
+
+	Image i = images[name];
+	render(NULL, &i.rect, i.filepath);
+}
+
+void Interface::setFont(std::string fontFile) {
+	
+	_font = TTF_OpenFont(fontFile.c_str(), _fontSize);
+
+	if (!_font)
+		std::cout << TTF_GetError() << std::endl;
+}
+
+void Interface::setFontSize(int size) {
+
+	_fontSize = size;
+}
+
+void Interface::displayText(std::string text, SDL_Rect* dstRect, SDL_Color color) {
+
+	_surface = TTF_RenderText_Solid(_font, text.c_str(), color);
+	_texture = SDL_CreateTextureFromSurface(_renderer, _surface);
+
+	if (_surface == NULL) {
+
+		std::cout << "Unable to render text surface! SDL_ttf Error: " << TTF_GetError() << std::endl;
+	}
+
+	// Prevent stretching of the text
+	dstRect->w = _surface->w;
+	dstRect->h = _surface->h;
+
+	SDL_RenderCopy(_renderer, _texture, NULL, dstRect);
+
+	SDL_FreeSurface(_surface);
+	SDL_DestroyTexture(_texture);
+}
+
+void Interface::displayRect(SDL_Rect* rect, SDL_Color color) {
+	
+	SDL_SetRenderDrawColor(_renderer, color.r, color.g, color.b, 0);
+	SDL_RenderFillRect(_renderer, rect);
+}
---
+++ Fishing Game/interface.h
@@ -0,0 +1,97 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <map>
+
+struct Image
+{
+	// Image filepath
+	std::string filepath;
+
+	// Clip of the source image
+	SDL_Rect rect;
+};
+
+class Button
+{
+public:
+	Button();
+	Button(SDL_Rect sRect, SDL_Rect hRect, SDL_Rect dRect, std::string path);
+	~Button();
+
+	// Sets where button is rendered on screen
+	void setDstRect(SDL_Rect dRect) {
+		dstRect = dRect;
+	}
+	
+	// Clip of the source image to be made into a button
+	SDL_Rect srcRect;
+
+	// Appearance of the images while mouse hovers over it
+	SDL_Rect hoverRect;
+
+	// Destination that button will be drawn on screen
+	SDL_Rect dstRect;
+
+	// Image filepath that button is from
+	std::string filePath;
+
+private:
+
+};
+
+class Interface
+{
+public:
+	// Window
+	int WINDOW_WIDTH = 1920;
+	int WINDOW_HEIGHT = 1080;
+	SDL_Point mousePos;
+
+	Interface();
+	~Interface();
+
+	void render(SDL_Rect* clip, SDL_Rect* dstRect, std::string image);
+	void renderFlip(SDL_Rect* clip, SDL_Rect* dstRect, std::string path, double angle, const SDL_Point* center, SDL_RendererFlip flip);
+	void clear();
+	void update();
+
+	// Window Functions
+	void setWindowWidth(int w);
+	void setWindowHeight(int h);
+
+	// Button Functions
+	void addButton(std::string button, SDL_Rect* srcRect, SDL_Rect* _hoverRect, SDL_Rect* dstRect, std::string filePath);
+	void displayButton(std::string button);
+	bool isButtonHovered(SDL_Point mousePos, std::string button);
+
+	// Image Functions
+	void addImage(std::string name, SDL_Rect* rect, std::string filePath);
+	void displayImage(std::string name);
+
+	// Text Functions
+	void setFont(std::string fontFile);
+	void setFontSize(int size);
+	void displayText(std::string text, SDL_Rect* dstRect, SDL_Color color);
+
+	// Draw other things functions
+	void displayRect(SDL_Rect* rect, SDL_Color color);
+	
+	// Holds images with key = image name
+	std::map<std::string, Image> images;
+
+	// Holds buttons with key = button name
+	std::map<std::string, Button> buttons; 
+
+private:
+
+	// Game UI
+	SDL_Window* _window;
+	SDL_Surface* _surface;
+	SDL_Texture* _texture;
+	SDL_Renderer* _renderer;
+
+	// Font
+	TTF_Font* _font;
+	int _fontSize;
+};
